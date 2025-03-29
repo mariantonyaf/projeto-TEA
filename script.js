de19456d4996a68fdb799b71d175f506bd389c0b
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-29 15:36:00 -0300

    pjt

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.getElementById("baixar-pdf").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "caminho/do/seu/arquivo.pdf"; // Substitua pelo caminho do arquivo PDF
+    link.download = "arquivo.pdf"; // Nome do arquivo a ser baixado
+    link.click();
+});
